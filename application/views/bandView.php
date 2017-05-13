7cd731419e4f332525e7ba2b08ea0db8a846008a
Shinyshade <pankov.anton.dmitrievich@gmail.com>
2017-05-13 14:00:47 +0400

    Contact info and EditProfile css added

--- application/views/bandView.php
+++ application/views/bandView.php
@@ -77,7 +77,12 @@
 ?>
 
 <div class="profile">
-    <div style="display: inline-block;">
+    <div style="display: inline-block; position: relative;">
+        <?php
+            if($owner) {
+                echo '<div style="position: absolute; top: 0px; right: 20px; width: 24px; height: 24px; padding: 0; margin: 0;"><a href=/user/profileBandEdit/'. $userName . '><img src="/images/x24/gear.png" alt="Edit profile"></a></div>';
+            }
+        ?>
         <div class="main-profile-info">
             <div class="logo-wrapper">
                 <?php
@@ -96,24 +101,51 @@
             </div>
         </div>
         <div class="profile-info">
-            <div>
-                <p style="margin: 0;">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
-                </p>
+            <div class="contact-info">
+                <h3 style="margin: 0;">Contact info</h3>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/global.png" alt="Website" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Website:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/facebook.png" alt="Facebook" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Facebook:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/instagram.png" alt="Instagram" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Instagram:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/twitter.png" alt="Twitter" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Twitter:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/skype.png" alt="Skype" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Skype:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/email.png" alt="Email" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Email:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
+                <div class="contact-info-row">
+                    <span class="contact-info-img"><img src="/images/x24/phone.png" alt="Phone" /></span>
+                    <?php echo ' <span class="contact-info-text""><b>Phone:</b> <a href="mailto:'. $email . '">'. $email . '</a></span>' ?>
+                </div>
             </div>
+            
             <div>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
+                    Enter Shikari are a British rock band formed in St Albans, Hertfordshire, England in 1999 under the name Hybryd by bassist Chris Batten, lead vocalist and keyboardist Roughton "Rou" Reynolds, and drummer Rob Rolfe. In 2003, guitarist Liam "Rory" Clewlow joined the band to complete its current lineup, and it adopted its current name. In 2006, they performed to a growing fanbase at Download Festival as well as a sold-out concert at the London Astoria. Their debut studio album, Take to the Skies, was released in 2007 and reached number 4 in the Official UK Album Chart, and has since been certified gold in the UK. Their second, Common Dreads, was released in 2009 and debuted on the UK Albums Chart at number 16;[2] while their third, A Flash Flood of Colour, was released in 2012 and debuted on the chart at number 4. Both have since been certified silver in the UK. The band spent a considerable amount of time supporting the latter release through the A Flash Flood of Colour World Tour, before beginning work on a fourth studio album, The Mindsweep, which was released in 2015.
+
+                    Enter Shikari have their own record label, Ambush Reality. However, they have also signed distribution deals with several major labels to help with worldwide distribution. Their eclectic musical style combines influences from rock, especially punk rock and hardcore punk, with those from various electronic music genres.
                 </p>
             </div>
         </div>        
     </div>
 
     <?php
-        echo  $email . '<br>';
         if($owner) {
-            echo '<a href=/user/uploadMusicBand/'. $userName . '>Upload Music</a><br>
-            <a href=/user/profileBandEdit/'. $userName . '>Edit profile</a><br>';
+            echo '<a class="button" href=/user/uploadMusicBand/'. $userName . '>Upload Music</a>';
         }
     ?>
     <textarea id="messageBody" placeholder="Message Body"></textarea>
