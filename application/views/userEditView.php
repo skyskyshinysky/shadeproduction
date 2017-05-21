6edcfdac367430b289bae0ef8ec09ab0d9f888c5
Shinyshade <pankov.anton.dmitrievich@gmail.com>
2017-05-21 23:52:50 +0400

    userView and upload music page css style

--- application/views/userEditView.php
+++ application/views/userEditView.php
@@ -1,8 +1,117 @@
 <?php
-var_dump($data);
-/**
- * Created by PhpStorm.
- * User: Ilya Pankov
- * Date: 21.05.2017
- * Time: 19:00
- */
+    extract($data);
+?>
+<form method="post" id="editUser" enctype="multipart/form-data">
+<div class="profile">
+    <div style="display: inline-block; position: relative;">
+        <div class="main-profile-info">
+            <div class="logo-wrapper">
+                <?php
+                    echo '<img class="profile-image" src="http://' . $_SERVER['HTTP_HOST']  . $logoBand['pathFile'] . $logoBand['nameLogo'] . '" />';
+                ?>
+            </div>
+            <div style="margin-top: 20px;">
+                <input type="file" name="uploadFileLogo" class="button" />
+            </div>
+            <div style="margin-top: 20px;">
+                <?php
+                    echo '<input class="form-input" name="firstName" style="text-align: center; max-width: 100%;" id="nameBand" placeholder="First Name" value="' . 'Ilya' . '" />';
+                ?>
+            </div>
+            <div style="margin-top: 20px;">
+                <?php
+                    echo '<input class="form-input" name="lastName" style="text-align: center; max-width: 100%;" id="nameBand" placeholder="Last Name" value="' . 'Pankov' . '" />';
+                ?>
+            </div>
+            <div style="margin-top: 20px;">
+                <?php
+                    echo '<input class="form-input" name="sex" style="text-align: center; max-width: 100%;" id="nameBand" placeholder="Sex" value="' . 'Male' . '" />';
+                ?>
+            </div>
+            <div style="margin-top: 20px;">
+                <?php
+                    echo '<input class="form-input" name="hometown" style="text-align: center; max-width: 100%;" id="nameBand" placeholder="Hometown" value="' . 'Votkinsk' . '" />';
+                ?>
+            </div>
+            <div style="margin-top: 20px;">
+                <?php
+                    echo '<input class="form-input" name="language" style="text-align: center; max-width: 100%;" id="nameBand" placeholder="Language" value="' . 'Russian' . '" />';
+                ?>
+            </div>
+        </div>
+         <div class="contact-info">
+            <h3 style="margin: 0;">Contact info</h3>
+            <div class="contact-info-row" style="margin-left: 0; margin-top: 10px;">
+                <span style="float: left;">
+                    <input class="form-input" name="website" type="text" size="30" maxlength="70" autocomplete="off" id="website" placeholder="Website..."
+                        <?php if(!empty($website)) {
+                            echo 'value = ' . $website;
+                        } ?>
+                    />
+                </span>
+                <span class="contact-info-img" style="float: left; margin-left: 10px; margin-top: 2px"><img src="/images/x24/global.png" alt="Website" /></span>
+            </div>
+            <div class="contact-info-row" style="margin-left: 0; margin-top: 10px;">
+                <span style="float: left;">
+                    <input class="form-input" name="facebook" type="text" size="30" maxlength="70" autocomplete="off" id="facebook" placeholder="Facebook..."
+                        <?php if(!empty($facebook)) {
+                            echo 'value = ' . $facebook;
+                        } ?>
+                    />
+                </span>
+                <span class="contact-info-img" style="float: left; margin-left: 10px; margin-top: 2px"><img src="/images/x24/facebook.png" alt="Facebook" /></span>
+            </div>
+            <div class="contact-info-row" style="margin-left: 0; margin-top: 10px;">
+                <span style="float: left;">
+                    <input class="form-input" name="instagram" type="text" size="30" maxlength="70" autocomplete="off" id="instagram" placeholder="Instagram..."
+                        <?php if(!empty($instagram)) {
+                            echo 'value = ' . $instagram;
+                        } ?>
+                    />
+                </span>
+                <span class="contact-info-img" style="float: left; margin-left: 10px; margin-top: 2px"><img src="/images/x24/instagram.png" alt="Instagram" /></span>
+            </div>
+            <div class="contact-info-row" style="margin-left: 0; margin-top: 10px;">
+                <span style="float: left;">
+                    <input class="form-input" name="twitter" type="text" size="30" maxlength="70" autocomplete="off" id="twitter" placeholder="Twitter..."
+                        <?php if(!empty($twitter)) {
+                            echo 'value = ' . $twitter;
+                        } ?>
+                    />
+                </span>
+                <span class="contact-info-img" style="float: left; margin-left: 10px; margin-top: 2px"><img src="/images/x24/twitter.png" alt="Twitter" /></span>
+            </div>
+            <div class="contact-info-row" style="margin-left: 0; margin-top: 10px;">
+                <span style="float: left;">
+                    <input class="form-input" name="skype" type="text" size="30" maxlength="70" autocomplete="off" id="skype" placeholder="Skype..."
+                        <?php if(!empty($skype)) {
+                            echo 'value = ' . $skype;
+                        } ?>
+                    />
+                </span>
+                <span class="contact-info-img" style="float: left; margin-left: 10px; margin-top: 2px"><img src="/images/x24/skype.png" alt="Skype" /></span>
+            </div>
+            <div class="contact-info-row" style="margin-left: 0; margin-top: 10px;">
+                <span style="float: left;">
+                    <input class="form-input email" name="email" type="text" size="30" maxlength="70" autocomplete="off" id="email" placeholder="Email..."
+                        <?php if(!empty($email)) {
+                            echo 'value = ' . $email;
+                        } ?>
+                    />
+                </span>
+                <span class="contact-info-img" style="float: left; margin-left: 10px; margin-top: 2px"><img src="/images/x24/email.png" alt="Email" /></span>
+            </div>
+        </div>
+    </div>
+    <div style="width: 100%; margin-top: 20px; position: relative;">
+        <div class="about">
+            <textarea class="comment-input form-input" name="aboutBand" id="aboutBand" placeholder="About Band">
+                <?php if(!empty($about)) {
+                    echo $about;
+                } ?>
+            </textarea>
+        </div>
+    </div>
+    <input style="margin-top: 20px;" class="button" type="submit" name="save" value="Save" />
+</div>
+</form>
--- application/views/userView.php
+++ application/views/userView.php
@@ -1,3 +1,98 @@
+<script type="text/javascript">
+    function sendMessageInUser() {
+        var now = new Date();
+        var textareaValue = $('#messageBody').val();
+        var postData = [];
+        postData["author"] = "Spiderman";
+        postData["datetime"] = now.toISOString();
+        postData["message"] = $('#messageBody').val();
+        $.ajax({
+            type: 'POST',
+            url: "/user/sendMessage",
+            data: $.extend({}, postData),
+            success: function(result) {
+                $('#messageBody').val('');
+                if(result != undefined && result != null) {
+
+                    var tr = $("<tr></tr>");
+
+                    var td = $('<td></td>').attr({
+                        style: 'padding: 6px 12px; vertical-align: top;',
+                    });
+                    var author = $("<b></b>");
+
+                    author.text('shadeproduction');
+                    author.appendTo(td);
+
+                    var datetime = $("<td></td>").attr({
+                        style: 'padding: 6px 12px; white-space: nowrap; vertical-align: top;',
+                    });
+                    datetime.text(now.toLocaleString());
+
+                    var message = $("<td></td>").attr({
+                        style: 'padding: 6px 12px; word-wrap: break-word; word-break: break-all;',
+                    });
+                    message.text(textareaValue);
+
+                    td.appendTo(tr);
+                    datetime.appendTo(tr);
+                    message.appendTo(tr);
+
+                    var tbody = $('table.wall tbody');
+                    tr.prependTo(tbody);
+                }
+            },
+            error: function(result) {
+                // todo: handle error;
+            },
+        });
+    }
+
+    function initializeComments()
+    {
+        //получаем в переменную data комменты
+        $.get('/user/getComments',function (data) {
+            //парсим JSON
+            var items = JSON.parse(data);
+            // строим и выводим каркас для 10 записей
+            for(var count = 0; count < items.length; count++) {
+
+                var tr = $("<tr></tr>");
+
+                var td = $('<td></td>').attr({
+                    style: 'padding: 6px 12px; vertical-align: top;',
+                });
+                var author = $("<b></b>");
+
+                author.text(items[count].userName);
+                author.appendTo(td);
+
+                var datetime = $('<td></td>').attr({
+                    style: 'padding: 6px 12px; white-space: nowrap; vertical-align: top;',
+                });
+                datetime.text(items[count].time);
+
+                var message = $('<td></td>').attr({
+                    style: 'padding: 6px 12px; word-wrap: break-word; word-break: break-all;',
+                });
+                message.text(items[count].message);
+
+                td.appendTo(tr);
+                datetime.appendTo(tr);
+                message.appendTo(tr);
+
+                var tbody = $('table.wall tbody');
+                tr.prependTo(tbody);
+            }
+        });
+    }
+
+    $(document).ready(function() {
+        $('#sendMessageInUser').on('click', sendMessageInUser);
+        initializeComments();
+    });
+</script>
+
 <?php
     extract($data);
 ?>
@@ -12,51 +107,76 @@
             </div>
             <div>
                 <?php
-                    echo '<h2 style="text-align: center;" id="nameBand">' . $firstName .  ' ' . $lastName . '</h2>';
+                    echo '<h2 style="text-align: center;" id="userName">' . 'Ilya Pankov' . '</h2>';
                 ?>
             </div>
             <div>
                 <?php
-                    echo '<h3 style="text-align: center;" id="genreMusic">' .$genreMusic . '</h3>';
+                    echo '<h3 style="text-align: center;" id="birthday">' . '16.02.1995' . '</h3>';
                 ?>
             </div>
             <div>
                 <?php
-                    echo '<h3 style="text-align: center;" id="origin">' . $origin . '</h3>';
+                    echo '<h3 style="text-align: center;" id="male">' . 'Male' . '</h3>';
+                ?>
+            </div>
+            <div>
+                <?php
+                    echo '<h3 style="text-align: center;" id="hometown">' . 'Votkinsk' . '</h3>';
+                ?>
+            </div>
+            <div>
+                <?php
+                    echo '<h3 style="text-align: center;" id="language">' . 'Russian' . '</h3>';
                 ?>
             </div>
             <?php
-            if($owner) {
-                echo '<div style="position: absolute; top: 10px; right: 10px; width: 24px; height: 24px; padding: 0; margin: 0;"><a href=/user/profileEdit/'. $userName . '><img src="/images/x24/gear.png" alt="Edit profile"></a></div>';
-            }
+                if($owner) {
+                    echo '<div style="position: absolute; top: 10px; right: 10px; width: 24px; height: 24px; padding: 0; margin: 0;"><a href=/user/profileUserEdit/'. $userName . '><img src="/images/x24/gear.png" alt="Edit profile"></a></div>';
+                }
             ?>
             <div class="contact-info">
                 <?php
-                if(!empty($website)) {
-                    echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://' . $website . '"><img src="/images/x24/global.png" alt="Website" /></a></span></div>';
-                }
-                if(!empty($facebook)) {
-                    echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://www.facebook.com/' . $facebook . '"><img src="/images/x24/facebook.png" alt="Facebook" /></a></span></div>';
-                }
-                if(!empty($instagram)) {
-                    echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://www.instagram.com/' . $instagram . '"><img src="/images/x24/instagram.png" alt="Instagram" /></a></span></div>';
-                }
-                if(!empty($twitter)) {
-                    echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://www.twitter.com/' . $twitter . '"><img src="/images/x24/twitter.png" alt="Twitter" /></a></span></div>';
-                }
-                if(!empty($skype)) {
-                    echo '<div class="contact-info-row"><span class="contact-info-img"><a href="skype:' . $skype . '?chat"><img src="/images/x24/skype.png" alt="Skype" /></a></span></div>';
-                }
-                if(!empty($email)) {
-                    echo '<div class="contact-info-row"><span class="contact-info-img"><a href="mailto:' . $email . '"><img src="/images/x24/email.png" alt="Email" /></a></span></div>';
-                }
+                    if(!empty($website)) {
+                        echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://' . $website . '"><img src="/images/x24/global.png" alt="Website" /></a></span></div>';
+                    }
+                    if(!empty($facebook)) {
+                        echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://www.facebook.com/' . $facebook . '"><img src="/images/x24/facebook.png" alt="Facebook" /></a></span></div>';
+                    }
+                    if(!empty($instagram)) {
+                        echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://www.instagram.com/' . $instagram . '"><img src="/images/x24/instagram.png" alt="Instagram" /></a></span></div>';
+                    }
+                    if(!empty($twitter)) {
+                        echo '<div class="contact-info-row"><span class="contact-info-img"><a href="http://www.twitter.com/' . $twitter . '"><img src="/images/x24/twitter.png" alt="Twitter" /></a></span></div>';
+                    }
+                    if(!empty($skype)) {
+                        echo '<div class="contact-info-row"><span class="contact-info-img"><a href="skype:' . $skype . '?chat"><img src="/images/x24/skype.png" alt="Skype" /></a></span></div>';
+                    }
+                    if(!empty($email)) {
+                        echo '<div class="contact-info-row"><span class="contact-info-img"><a href="mailto:' . $email . '"><img src="/images/x24/email.png" alt="Email" /></a></span></div>';
+                    }
                 ?>
             </div>
         </div>
         <div class="about">
-            <p style="margin: 0;">
-                <?php echo $about; ?>
-            </p>
+                <p style="margin: 0;">
+                    <?php echo $about; ?>
+                </p>
         </div>
+    </div>
+    <div style="width: 100%; margin-top: 20px; position: relative;">
+        <h3 style="margin: 0;">Comment area</h3>
+        <table class="wall">
+            <tbody>
+
+            </tbody>
+        </table>
+        <ul id="page-numbers"></ul>
+        <div class="center">
+            <div class="pagination">
+            </div>
+        </div>
+        <textarea class="comment-input form-input" id="messageBody" placeholder="Message Body"></textarea>
+        <button style="margin-top: 10px;" onclick="sendMessageInUser"  class="button" id="sendMessageInUser">Send message</button>
     </div>
 </div>
